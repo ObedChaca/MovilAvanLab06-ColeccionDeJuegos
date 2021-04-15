f51e320b57a227458f2177779c5202ff6d48d7cf
ObedChaca <obed.chaca@tecsup.edu.pe>
2021-04-15 10:52:50 -0500

    first commit

---
+++ lab06-ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab06-ColeccionDeJuegos
+//
+//  Created by mbtec22 on 4/15/21.
+//  Copyright © 2021 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
